7dd78b4bf3530c660f8d8d9ec3468a6ef65b7d85
tonyshh <shalin.anton8@gmail.com>
2023-12-03 16:10:01 +0700

    fix

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 make install
 
 # Apply database migrations
-psql -a -d $DATABASE_URL -f database.sql
+psql -a -d postgres://tonysh:aWq4BI4O1ZGeGZuUa0MTFPwe4Hu431dt@dpg-clm45b9fb9qs7397b2cg-a.singapore-postgres.render.com/page_analyzer_mvmf -f database.sql
